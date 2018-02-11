661d978bc31b22d06515c3ce756d2549d75fcb7d
Dryamov Vyacheslav <dryamov.dev@gmail.com>
2018-02-11 09:04:21 +0300

    =(

--- libs/pam_snapper_useradd.sh
+++ libs/pam_snapper_useradd.sh
@@ -14,23 +14,23 @@ CMD_SNAPPER="/usr/bin/snapper"
 CMD_EGREP="grep -E"
 CMD_PAM_CONFIG="/usr/sbin/pam-config"
 CMD_SED="sed"
-CMD_USERADD="/usr/bin/useradd"
+CMD_USERADD="useradd -m"
 CMD_USERDEL="userdel -r"
-CMD_CHOWN="chown -R"
-CMD_CHMOD="chmod -R"
+CMD_CHOWN="chown"
+CMD_CHMOD="chmod"
 #
 SNAPPERCFGDIR="/etc/snapper/configs"
-HOMEHOME="/home"
+HOMEHOME=/home
 DRYRUN=0
-#MYUSER=${new_user}
-#MYGROUP=${new_user_group}
-#if [ ".${MYGROUP}" == "." ] ; then MYGROUP="${MYUSER}"; fi
-#
+MYUSER=$1
+MYGROUP=$2
+if [ ".${MYGROUP}" == "." ] ; then MYGROUP="users"; fi
+
 # Usage
-#if [ "0$MYUSER" == "0" ]; then
-#	echo "USAGE: $0 <username> [group]"
-#	exit 1
-#fi
+if [ "0$MYUSER" == "0" ]; then
+	echo "USAGE: $0 <username> [group]"
+	exit 1
+fi
 
 # Sanity-Check: ist $HOMEHOME a btrfs filesystem
 ${CMD_BTRFS} filesystem df ${HOMEHOME} 2>&1 > /dev/null
@@ -42,18 +42,17 @@ fi
 
 if [ ${DRYRUN} == 0 ] ; then
 	# Create subvolume for USER
-        ${CMD_BTRFS} subvol create ${HOMEHOME}/${new_user}
+	${CMD_BTRFS} subvol create ${HOMEHOME}/${MYUSER}
 	# Create snapper config for USER
-	${CMD_SNAPPER} -c home_${new_user} create-config ${HOMEHOME}/${new_user}
-	 ${CMD_SED} -i -e "s/ALLOW_USERS=\"\"/ALLOW_USERS=\"${new_user}\"/g" ${SNAPPERCFGDIR}/home_${new_user}
+	${CMD_SNAPPER} -c home_${MYUSER} create-config ${HOMEHOME}/${MYUSER}
+	${CMD_SED} -i -e "s/ALLOW_USERS=\"\"/ALLOW_USERS=\"${MYUSER}\"/g" ${SNAPPERCFGDIR}/home_${MYUSER}
 	# Create USER
-	 "${CMD_USERADD} ${new_user}"
+	${CMD_USERADD} ${MYUSER}
 	# yast users add username=${MYUSER} home=/home/${MYUSER} password=""
 	# !! IMPORTANT !!
 	# chown USER's home directory
-	cp -R /etc/skel/.config  ${HOMEHOME}${new_user}
-	${CMD_CHOWN} ${new_user}.${new_user_group} ${HOMEHOME}/${new_user}
-	${CMD_CHMOD} 755 ${HOMEHOME}/${new_user}/.snapshots
+	${CMD_CHOWN} ${MYUSER}.${MYGROUP} ${HOMEHOME}/${MYUSER}
+	${CMD_CHMOD} 755 ${HOMEHOME}/${MYUSER}/.snapshots
 else
 	echo -e "#"
 	echo "DRYRUN - no changes submitted"
