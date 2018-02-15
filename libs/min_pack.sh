f3b6945f66f4537ce2ad21a5015724335de95bc2
Dryamov Vyacheslav <dryamov.dev@gmail.com>
2018-02-15 07:54:07 +0300

    Refactored systemd first setup

--- libs/min_pack.sh
+++ libs/min_pack.sh
@@ -8,7 +8,7 @@ min_pack(){
   pacstrap  -C configs/etc/alis/pacstrap.conf "$mountpoint" base systemd-swap crda polkit linux linux-headers mkinitcpio \
                                                     btrfs-progs snapper intel-ucode vulkan-intel libva-intel-driver broadcom-wl-dkms wpa_supplicant \
                                                     terminus-font zsh zsh-completions zsh-syntax-highlighting bash-completion \
-                                                    gzip sed lzop nano iputils sudo wget  \
+                                                    gzip sed lzop nano iputils sudo wget efibootmgr efitools sbsigntools \
                                                     dhcpcd man iproute2 pkgfile
 }
 
