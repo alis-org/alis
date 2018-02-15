ddba72166806d85bc5de6efbe17584fb54a1fd6b
Dryamov Vyacheslav <dryamov.dev@gmail.com>
2018-02-15 05:08:18 +0300

    some

--- libs/min_pack.sh
+++ libs/min_pack.sh
@@ -6,9 +6,9 @@ min_pack(){
   local name="installing minimal required packages"
   title "Start $name: $@"
   pacstrap  -C configs/etc/alis/pacstrap.conf "$mountpoint" base systemd-swap crda polkit linux linux-headers mkinitcpio \
-                                                    btrfs-progs snapper intel-ucode broadcom-wl-dkms wpa_supplicant \
+                                                    btrfs-progs snapper intel-ucode vulkan-intel libva-intel-driver broadcom-wl-dkms wpa_supplicant \
                                                     terminus-font zsh zsh-completions zsh-syntax-highlighting bash-completion \
-                                                    gzip sed lzop nano iputils sudo wget intel-ucode \
+                                                    gzip sed lzop nano iputils sudo wget  \
                                                     dhcpcd man iproute2 pkgfile
 }
 
