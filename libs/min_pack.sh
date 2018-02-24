49c64f81f0d259a9a8caa6fa077243b300303ca2
Dryamov Vyacheslav <dryamov.dev@gmail.com>
2018-02-24 07:11:03 +0300

    fixed somesettings in systemd

--- libs/min_pack.sh
+++ libs/min_pack.sh
@@ -7,7 +7,7 @@ min_pack(){
   title "Start $name: $@"
   pacstrap  -C configs/etc/alis/pacstrap.conf "$mountpoint" base systemd-swap crda polkit linux linux-headers mkinitcpio \
                                                     btrfs-progs snapper snapper-gui-git intel-ucode vulkan-intel libva-intel-driver broadcom-wl-dkms wpa_supplicant \
-                                                    wpa_supplicant_gui terminus-font ttf-dejavu zsh zsh-completions zsh-syntax-highlighting bash-completion \
+                                                    wpa_supplicant_gui terminus-font ttf-dejavu ttf-hack  zsh zsh-completions zsh-syntax-highlighting bash-completion \
                                                     gzip sed lzop nano iputils sudo wget efibootmgr efitools sbsigntools \
                                                     man iproute2 pkgfile xdelta3 grc modprobed-db plymouth systemd-boot-password \
                                                     libcanberra-pulse libcanberra-gstreamer gst-plugins-base gst-plugins-good gst-libav profile-sync-daemon  \
